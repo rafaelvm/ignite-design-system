c1421a7d9a6063d7ad4fe2c1d139f41772ed55df
rafaelvm <vieiramrafael@gmail.com>
2022-10-13 22:21:58 -0300

    feat: created login page

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,18 +5,28 @@ export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: React.ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({ children, size = "md", asChild }: HeadingProps) {
+export function Heading({
+  children,
+  size = "md",
+  asChild,
+  className,
+}: HeadingProps) {
   const Comp = asChild ? Slot : "h2";
 
   return (
     <Comp
-      className={clsx("text-gray-100 font-bold font-sans", {
-        "text-lg": size === "sm",
-        "text-xl": size === "md",
-        "text-2xl": size === "lg",
-      })}
+      className={clsx(
+        "text-gray-100 font-bold font-sans",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "md",
+          "text-2xl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Comp>
